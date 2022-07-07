80db6eea47682d287f00b095c5585cec245fa825
davnig <daddets97@gmail.com>
2022-07-07 23:14:02 +0200

    refactor: rename variables

--- baseline/training_baseline.py
+++ baseline/training_baseline.py
@@ -1,27 +1,19 @@
-import pandas as pd
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
 
 from baseline.dataset import SerieAMatchesDataset
 from baseline.model import MLP
+from utils import count_features
 
-learning_rate = 0.001
-num_epochs = 20
-
-
-def count_features(data_csv: str):
-    df = pd.read_csv(data_csv, nrows=1)
-    n_of_features = len(df.columns)
-    del df
-    return n_of_features
-
+LEARNING_RATE = 0.001
+NUM_EPOCHS = 20
 
 if __name__ == '__main__':
     csv_name = 'data_baseline.csv'
     n_of_feats = count_features(csv_name)
     dataset = SerieAMatchesDataset(csv_file=csv_name)
-    model = MLP(dataset=dataset, input_size=n_of_feats, learning_rate=learning_rate)
+    model = MLP(dataset=dataset, input_size=n_of_feats, learning_rate=LEARNING_RATE)
     logger = TensorBoardLogger("../lightning_logs", name="baseline_results")
-    trainer = Trainer(gpus=1, max_epochs=num_epochs, logger=logger)
+    trainer = Trainer(gpus=1, max_epochs=NUM_EPOCHS, logger=logger)
     trainer.fit(model)
     trainer.test(model)
--- hybrid/training_hybrid.py
+++ hybrid/training_hybrid.py
@@ -1,34 +1,26 @@
-import pandas as pd
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
 from torchsummary import summary
 
 from hybrid.dataset import SerieAMatchesWithHistoryDataset
 from hybrid.model import HybridRNN, HybridMLP, HybridNetwork
+from utils import count_features
 
-learning_rate = 0.001
-num_epochs = 20
-hidden_size = 128
-
-
-def count_features(data_csv: str):
-    df = pd.read_csv(data_csv, nrows=1)
-    n_of_features = len(df.columns)
-    del df
-    return n_of_features
-
+LEARNING_RATE = 0.001
+NUM_EPOCHS = 20
+HIDDEN_SIZE = 128
+CSV_NAME = 'data1.csv'
 
 if __name__ == '__main__':
-    csv_name = 'data1.csv'
-    tot_num_of_feats = count_features(csv_name)
-    dataset = SerieAMatchesWithHistoryDataset(csv_file=csv_name)
-    rnn_home = HybridRNN(input_size=tot_num_of_feats, hidden_size=hidden_size)
-    rnn_away = HybridRNN(input_size=tot_num_of_feats, hidden_size=hidden_size)
-    mlp = HybridMLP(hidden_size * 2 + tot_num_of_feats - 5)
+    tot_num_of_feats = count_features(CSV_NAME)
+    dataset = SerieAMatchesWithHistoryDataset(csv_file=CSV_NAME)
+    rnn_home = HybridRNN(input_size=tot_num_of_feats, hidden_size=HIDDEN_SIZE)
+    rnn_away = HybridRNN(input_size=tot_num_of_feats, hidden_size=HIDDEN_SIZE)
+    mlp = HybridMLP(HIDDEN_SIZE * 2 + tot_num_of_feats - 5)
     model = HybridNetwork(dataset=dataset, rnn_home_model=rnn_home, rnn_away_model=rnn_away, mlp_model=mlp,
-                          learning_rate=learning_rate)
+                          learning_rate=LEARNING_RATE)
     print(summary(model))
     logger = TensorBoardLogger("../lightning_logs", name="hybrid_results")
-    trainer = Trainer(fast_dev_run=False, gpus=1, max_epochs=num_epochs, logger=logger)
+    trainer = Trainer(fast_dev_run=False, gpus=1, max_epochs=NUM_EPOCHS, logger=logger)
     trainer.fit(model)
     trainer.test(model)
